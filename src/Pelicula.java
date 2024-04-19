b4677d84c961780aecc468b816acdb1aed412596
Diegodfp <diegodfp.fly@gmail.com>
2024-04-19 12:23:06 -0500

    ok

--- src/Pelicula.java
+++ src/Pelicula.java
@@ -1,2 +1,7 @@
-package PACKAGE_NAME;public class Pelicula {
+public class Pelicula {
+    String nombre;
+    int fechaDeLanzamiento;
+    int duracionEnMinutos;
+    boolean incluidoEnElPlan;
+
 }
